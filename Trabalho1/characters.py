0abce36fb2000bd904d5c61af83d8958ea1a8ed9
caefleury <caefleury15@gmail.com>
2024-01-29 17:13:33 -0300

    feat: hero and enemy mechanics

---
+++ Trabalho1/characters.py
@@ -0,0 +1,60 @@
+from typing import Any
+import random
+
+class Character:
+    def __init__(self, name, life, level) -> None:
+        self.__name = name
+        self.__life = life
+        self.__level = level
+
+    def get_name(self):
+        return self.__name
+
+    def get_life(self):
+        return self.__life
+
+    def get_level(self):
+        return self.__level
+
+    def show_details(self):
+        return f"Nome: {self.get_name()}\nVida: {self.get_life()}\nNível: {self.get_level()}"
+    
+    def attack(self,target):
+        damage = self.__level * 2
+        target.receive_attack(damage)
+        print(f"\n{self.get_name()} atacou {target.get_name()} e causou {damage} de dano")
+
+    def special_attack(self,target):
+        damage = self.__level * 5
+        target.receive_attack(damage)
+        print(f"\n{self.get_name()} atacou {target.get_name()} com um ataque espcial e causou {damage} de dano")
+
+    def receive_attack(self,damage):
+        self.__life -= damage
+        if self.__life < 0:
+            self.__life = 0
+
+
+class Hero(Character):
+    def __init__(self, name, life, level, special_attack) -> None:
+        super().__init__(name, life, level)
+        self.__special_attack = special_attack
+
+    def get_special_attack(self):
+        return self.__special_attack
+
+    def show_details(self):
+        return f"{super().show_details()}\nHabilidade: {self.get_special_attack()}"
+
+
+class Enemy(Character):
+    def __init__(self, name, life, level, type) -> None:
+        super().__init__(name, life, level)
+        self.__type = type
+
+    def get_type(self):
+        return self.__type
+
+    def show_details(self):
+        return f"{super().show_details()}\nTipo: {self.get_type()}"
+    
--- Trabalho1/main.py
+++ Trabalho1/main.py
@@ -3,75 +3,40 @@
 # Adversário: controlado pelo usúario
 
 from typing import Any
-
-
-class Character:
-    def __init__(self, name, life, level) -> None:
-        self.__name = name
-        self.__life = life
-        self.__level = level
-
-    def get_name(self):
-        return self.__name
-
-    def get_life(self):
-        return self.__life
-
-    def get_level(self):
-        return self.__level
-
-    def show_details(self):
-        return f"Nome: {self.get_name()}\nVida: {self.get_life()}\nNível: {self.get_level()}"
-
-
-class Hero(Character):
-    def __init__(self, name, life, level, special_attack) -> None:
-        super().__init__(name, life, level)
-        self.__special_attack = special_attack
-
-    def get_special_attack(self):
-        return self.__special_attack
-
-    def show_details(self):
-        return f"{super().show_details()}\nHabilidade: {self.get_special_attack()}"
-
-
-class Enemy(Character):
-    def __init__(self, name, life, level, type) -> None:
-        super().__init__(name, life, level)
-        self.__type = type
-
-    def get_type(self):
-        return self.__type
-
-    def show_details(self):
-        return f"{super().show_details()}\nTipo: {self.get_type()}"
-
-
-# heroi = Hero(name='Sergio Sacani', life=100,
-#              level=164, special_attack="Foguetes")
-# print(heroi.show_details())
-
-# enemy = Enemy(name='Terraplanista genérico', life=60, level=230, type='Burro')
-# print(enemy.show_details())
+from characters import Character, Hero, Enemy
 
 class Game:
     def __init__(self) -> None:
-        self.hero = Hero(name='Sergio Sacani', life=100, level=164, special_attack="Foguetes")
-        self.enemy = Enemy(name='Terraplanista genérico', life=60, level=230, type='Burro')
+        self.hero = Hero(name='Sergio Sacani', life=100, level=5, special_attack="Foguetes")
+        self.enemy = Enemy(name='Terraplanista genérico', life=60, level=3, type='Burro')
 
     def start_battle(self):
         """ Gestão de batalhas em turnos """
         print("Iniciando batalha")
         while self.hero.get_life() > 0 and self.enemy.get_life() > 0:
-            print("Detalhes dos personagens")
-            print("Detalhes do heroi")
+            print("\nDetalhes dos personagens")
+            print("\nDetalhes do heroi")
             print(self.hero.show_details())
-            print("Detalhes do inimigo ")
+            print("\nDetalhes do inimigo ")
             print(self.enemy.show_details())
 
             input("Pressione Enter para atacar....")
-            escolha = input("Escolha (1 -  Ataque Normal, 2 - Ataque especial): ")
+            choice = input("Escolha (1 -  Ataque Normal, 2 - Ataque especial): ")
+
+            if choice == '1':
+                self.hero.attack(self.enemy)
+            elif choice == '2':
+                self.hero.special_attack(self.enemy)
+            else:
+                print("Escolha invalida, escolha novamente")
+        
+            if self.enemy.get_life() > 0:
+                self.enemy.attack(self.hero)
+
+        if self.hero.get_life() > 0:
+            print(f"Parabens {self.hero.get_name()}, você venceu a batalha!")
+        else:
+            print("Que pena, o inimigo venceu a batalha")
 
 #Criar instância do jogo e inciar batalha
 jogo = Game()
