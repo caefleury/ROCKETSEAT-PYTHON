7dacb970c5c3dd4206f0441b688c685cf2d3a651
caefleury <caefleury15@gmail.com>
2024-01-30 18:24:06 -0300

    style: fix folder order

---
+++ Desafio 1/main.py
@@ -0,0 +1,12 @@
+
+class ContactManager:
+    def start_manager():
+        print("Iniciando o ContactManager")
+        while True:
+            print("\nEscolha uma opção:")
+            print("1 - Adicionar contato")
+            print("2 - Listar contatos")
+            print("3 - Listar contatos favoritos")
+            print("4 - Remover contato")
+            print("5 - Sair")
+            choice = input("Escolha: ")
