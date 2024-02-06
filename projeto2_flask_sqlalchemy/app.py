da25566b47c034a907d9c98973e6b8d3bec72d8b
Caetano <caefleury15@gmail.com>
2024-02-06 18:54:43 -0300

    feat: user login method

--- projeto2_flask_sqlalchemy/app.py
+++ projeto2_flask_sqlalchemy/app.py
@@ -3,6 +3,7 @@ from database import db
 from dotenv import load_dotenv
 import os
 from models.user import User
+from flask_login import LoginManager
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,8 +13,20 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
-
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            return jsonify({"message": "Usuario logado com sucesso!"}), 200
+    return jsonify({"message": "Credenciais invalidas"}), 400
 
 
 @app.route('/users', methods=['GET'])
