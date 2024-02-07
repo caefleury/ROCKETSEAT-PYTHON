364b08bf8543d172fef97b0859e15c9b2c383e1c
Caetano <caefleury15@gmail.com>
2024-02-07 18:08:13 -0300

    feat: user login request

--- projeto2/app.py
+++ projeto2/app.py
@@ -3,7 +3,7 @@ from database import db
 from dotenv import load_dotenv
 import os
 from models.user import User
-from flask_login import LoginManager
+from flask_login import LoginManager,login_user,current_user
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,6 +17,11 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 @app.route("/login", methods=['POST'])
 def login():
     data = request.json
@@ -25,6 +30,8 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message": "Usuario logado com sucesso!"}), 200
     return jsonify({"message": "Credenciais invalidas"}), 400
 
