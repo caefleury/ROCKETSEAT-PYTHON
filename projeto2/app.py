c726ec4327995f5c2dc3f2f9b7d154fc4ced0bd0
Caetano <caefleury15@gmail.com>
2024-02-07 18:32:32 -0300

    feat: user logout method

--- projeto2/app.py
+++ projeto2/app.py
@@ -3,7 +3,7 @@ from database import db
 from dotenv import load_dotenv
 import os
 from models.user import User
-from flask_login import LoginManager,login_user,current_user
+from flask_login import LoginManager,login_user,current_user,logout_user,login_required
 
 # Load environment variables from .env file
 load_dotenv()
@@ -21,7 +21,6 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(user_id)
 
-
 @app.route("/login", methods=['POST'])
 def login():
     data = request.json
@@ -31,14 +30,15 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             login_user(user)
-            print(current_user.is_authenticated)
             return jsonify({"message": "Usuario logado com sucesso!"}), 200
     return jsonify({"message": "Credenciais invalidas"}), 400
 
 
-@app.route('/users', methods=['GET'])
-def hello():
-    return jsonify({'message': 'Hello, World!'})
+@app.route("/logout",methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message":"Usuário deslogado com sucesso"})
 
 
 if __name__ == '__main__':
